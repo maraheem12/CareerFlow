8f1803dcd3257739b0cf886da770b97da8ce428c
Mohammed Abdul Raheem <maraheem812@gmail.com>
2025-09-07 23:17:22 +0530

    tailwind and react is setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="bg-amber-400">
+        <h1>Shaik Raheem Porfolio</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
