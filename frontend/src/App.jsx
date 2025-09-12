b3d4c24a9891a1bbfc976753d85c7a3714cc04f0
Mohammed Abdul Raheem <maraheem812@gmail.com>
2025-09-12 16:03:42 +0530

    context store is created

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,20 @@
 import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import ApplyJob from "./pages/ApplyJob";
+import Applications from "./pages/Applications";
+import Start from "./pages/start";
 
 function App() {
   return (
-    <>
-      <div className="bg-amber-400">
-        <h1>Shaik Raheem Porfolio</h1>
-      </div>
-    </>
+    <div>
+      <Routes>
+        <Route path="/" element={<Start/>} />
+        <Route path="/Home" element={<Home/>} />
+        <Route path="/apply-job/:id" element={<ApplyJob/>} />
+        <Route path="applications" element={<Applications/>} />
+      </Routes>
+    </div>
   );
 }
 
