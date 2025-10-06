3dd88849bc741d73f54752503dd52f3db06f12b5
maraheem12 <maraheem812@gmail.com>
2025-10-06 11:25:05 +0530

    MongoDb URL is Placed

--- backend/server.js
+++ backend/server.js
@@ -1,2 +1,19 @@
-//server will be done by Raheem and uzair
+import express from "express"
+import cors from "cors"
+import "dotenv/config"
+
+// Initialize express
+const app = express();
+
+//Middlewares 
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (req, res) => res.send("API Working") )
+
+const PORT = 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server is listening of port ${PORT}`)
+})
 
