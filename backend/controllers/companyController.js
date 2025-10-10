0a235864918592b100862bd0cf75527eafc1833d
maraheem12 <maraheem812@gmail.com>
2025-10-10 20:00:02 +0530

    Company Controller is created

---
+++ backend/controllers/companyController.js
@@ -0,0 +1,21 @@
+
+//Register a Comapany
+const registerComapany = async (req, res) =>{
+
+}
+
+//Login a company
+const loginCompany = async (req, res) => {
+
+}
+
+//Get company Data
+const getCompanyData = async (re, res) => {
+
+}
+
+//Post job
+const postJob = async (req, res) => {
+
+} 
+
